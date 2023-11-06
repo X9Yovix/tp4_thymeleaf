13fbb884ad97e0c3bd80c95b00b21933fa691606
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-06 17:21:36 +0100

    list of elements

--- src/main/java/com/tekup/thymeleaf/controller/AppController.java
+++ src/main/java/com/tekup/thymeleaf/controller/AppController.java
@@ -4,17 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class AppController {
 
     @GetMapping("/")
-    public String mainPage(Model model){
-        model.addAttribute("app","Thymeleaf");
+    public String mainPage(Model model) {
+        model.addAttribute("app", "Thymeleaf");
         return "index";
     }
 
     @GetMapping("/*")
-    public String notFound(Model model){
+    public String notFound(Model model) {
         return "error_404";
+    }
+
+    @GetMapping("/elements")
+    public String elements(Model model) {
+        List<String> list = Arrays.asList("Element 1", "Element 2", "Element 3");
+        model.addAttribute("elements",list);
+        return "list";
     }
 }
