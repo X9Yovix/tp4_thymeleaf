f23c42652ee44c92df7c275ffd63ed1eee5c3087
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-06 17:05:37 +0100

    add attribute to the model

---
+++ src/main/java/com/tekup/thymeleaf/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.tekup.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String mainPage(Model model){
+        model.addAttribute("app","Thymeleaf");
+        return "index";
+    }
+}
